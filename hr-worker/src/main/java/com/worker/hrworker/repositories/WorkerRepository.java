06108515d2f98b97cf9484c4fb871e569609a2cb
Remyson <remysonrodrigues@gmail.com>
2022-07-23 10:23:59 -0300

    Projeto ms-curso

--- hr-worker/src/main/java/com/worker/hrworker/repositories/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.worker.hrworker.repositories;
-
-import com.worker.hrworker.entities.Worker;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
-}
